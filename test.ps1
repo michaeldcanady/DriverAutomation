5ba714c04bd061c01052595b687b8e296b9efffa
michaeldcanady <michaeldcanady@yahoo.com>
2021-12-05 22:13:48 -0500

    Added ScriptInfo

--- test.ps1
+++ test.ps1
@@ -1,48 +1,3 @@
-<#PSScriptInfo
-
-.VERSION 1.1.0.0
-
-.GUID 645bfabe-c85f-4fe4-8558-06934cfff302
-
-.AUTHOR Michael Canady
-
-.COMPANYNAME
-
-.COPYRIGHT 2021 Michael Canady. All rights reserved
-
-.TAGS
-
-.LICENSEURI https://github.com/michaeldcanady/DriverAutomation/blob/master/LICENSE
-
-.PROJECTURI https://github.com/michaeldcanady/DriverAutomation
-
-.ICONURI
-
-.EXTERNALMODULEDEPENDENCIES 
-
-.REQUIREDSCRIPTS
-
-.EXTERNALSCRIPTDEPENDENCIES
-
-.RELEASENOTES Adding Parameters (All), Structing Process Block
-
-.PRIVATEDATA
-
-#> 
-
-#>
-
-
-<#
-.SYNOPSIS
-
-.DESCRIPTION
-A Tool to Generate Driver Packages to be Used in Configuration Manager Task Sequences.<Overview of script><Overview of script><Overview of script>
-
-.PARAMETER Path
-None
-    
-.INPUTS
 <Inputs if any, otherwise state None>
 
 .OUTPUTS
@@ -54,17 +9,26 @@ Author:         Michael Canady
 Creation Date:  11/24/2021
 Purpose/Change: Initial Script Development
 
-Version:        1.1.0.0
+Version:        1.0.1.0
 Author:         Michael Canady
 Creation Date:  11/30/2021
 Purpose/Change: Adding Parameters (All), Structing Process Block
+
+Version:        1.0.2.0
+Author:         Michael Canady
+Creation Date:  12/05/2021
+Purpose/Change: Changed formatting of script
+
+Version:        1.1.0.0
+Author:         Michael Canady
+Creation Date:  12/05/2021
+Purpose/Change: Added link for the license
   
 .EXAMPLE
 BackEndv2
 #>
 
 #-----------------------------------------------------------[Parameters]-----------------------------------------------------------
-
 [CmdletBinding()]
 param(
     [Parameter(ParameterSetName = "Download")]
@@ -136,6 +100,9 @@ param(
 )
 
 BEGIN {
+    #Gets version of the script
+    $ScriptVersion = New-Object System.Version((Test-ScriptFileInfo -Path $PSCommandPath).version)
+
     #---------------------------------------------------------[Initialisations]--------------------------------------------------------
 
     Import-Module .\Packages\FileHandling\FileHandling.psm1 -Force
@@ -143,6 +110,81 @@ BEGIN {
     Import-Module .\Packages\WindowsVersions\WindowsVersions.psm1 -Force
 
     #----------------------------------------------------------[Functions]-------------------------------------------------------------
+    function Update-DriverAutomation {
+        [CmdletBinding(SupportsShouldProcess = $true)]
+        param(
+            [switch]$Force
+        )
+
+        $Params = @{
+            Method = "Get"
+            Uri    = "https://api.github.com/repos/michaeldcanady/DriverAutomation/releases/latest"
+        }
+        
+        #Need name, published_at, prerelease, tag_name, zipball_url, draft, body, url
+        $LatestVersion = Invoke-RestMethod @Params | Select-Object name, published_at, prerelease, tag_name, assets, draft, body, @{N = "url"; E = { $_.assets.browser_download_url } }, @{N = "Author"; E = { $_.author.login } }
+        
+        $NormalizedLatestVersion = New-Object System.Version($LatestVersion.tag_name)
+    
+        if ($ScriptVersion -ge $NormalizedLatestVersion) {
+            return $false
+        }
+
+        Write-Host "New version: [$NormalizedLatestVersion] found. Current: [$ScriptVersion]"
+
+        $title = 'something'
+        $question = "Would you like to install it?"
+        $choices = '&Yes', '&No'
+
+        $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
+
+        if ($decision -ne 0) {
+            return $false
+        }
+        
+        try {
+            Write-Verbose "Downloading Version [$NormalizedLatestVersion]..."
+        
+            $DestFile = ($LatestVersion.url).split("/")[-1]
+            $DestPath = "$TEMP\$DestFile"
+        
+            Start-BitsTransfer -Source $LatestVersion.url
+        
+            $DriverAutomation = "$psscriptroot\DriverAutomationTool.exe"
+            $DriverAutomationOld = "$psscriptroot\DriverAutomationTool.exe.old"
+        
+            Write-Verbose "Renaming $DriverAutomation to $DriverAutomationOld"
+        
+            Rename-Item -LiteralPath "$DriverAutomation" -NewName "$DriverAutomationOld"
+        
+            Write-Verbose "Copying $DestPath to $psscriptroot"
+        
+            Copy-Item -Path $DestPath -Destination $psscriptroot
+        
+            Write-Verbose "Removing $DriverAutomationOld"
+        
+            Remove-Item -Path $DriverAutomationOld
+        
+            Write-Verbose "Successfully updated Driver Automation"
+        }
+        catch {
+            Write-Error $_.Exception.Message
+
+            if (Test-Path $DestPath) {
+                Write-Verbose "Removing $DestPath"
+                Remove-Item $DestPath
+            }
+        
+            if (!(Test-Path $DriverAutomation) -and (Test-Path $DriverAutomationOld)) {
+                Write-Verbose "Renaming $DriverAutomationOld to $DriverAutomation"
+                Rename-Item -Path $DriverAutomationOld -NewName $DriverAutomation
+            }
+
+            return $false
+        }
+
+        return $true
+    }
 
     function Get-ManufacturersInformation {
         <#
@@ -214,83 +256,6 @@ BEGIN {
         }
     }
 
-    function Update-DriverAutomation {
-        param(
-            $NormalizedScriptVersion,
-            $Temp = "C:\Temp"
-        )
-    
-        $Params = @{
-            Method = "Get"
-            Uri    = "https://api.github.com/repos/michaeldcanady/DriverAutomation/releases/latest"
-        }
-        
-        #Need name, published_at, prerelease, tag_name, zipball_url, draft, body, url
-        $LatestVersion = Invoke-RestMethod @Params | Select-Object name, published_at, prerelease, tag_name, assets, draft, body, @{N = "url"; E = { $_.assets.browser_download_url } }, @{N = "Author"; E = { $_.author.login } }
-        
-        $NormalizedLatestVersion = New-Object System.Version($LatestVersion.tag_name)
-        
-        if ($NormalizedScriptVersion -ge $NormalizedLatestVersion) {
-            return $false
-        }
-    
-        Write-Host "New version: [$NormalizedLatestVersion] found. Current: [$NormalizedScriptVersion]"
-    
-        $title = 'Update Confirmation'
-        $question = 'Would you like to install it?'
-        $choices = '&Yes', '&No'
-    
-        $decision = $Host.UI.PromptForChoice($title, $question, $choices, 1)
-    
-        if ($decision -ne 0) {
-            return $false
-        }
-    
-        try {
-            Write-Verbose "Downloading Version [$NormalizedLatestVersion]..."
-    
-            $DestFile = ($LatestVersion.zipball_url).split("/")[-1]
-            $DestPath = "$Temp\$DestFile"
-    
-            Start-BitsTransfer -Source $LatestVersion.url
-    
-            $DriverAutomation = "$psscriptroot\DriverAutomationTool.exe"
-            $DriverAutomationOld = "$psscriptroot\DriverAutomationTool.exe.old"
-    
-            Write-Verbose "Renaming $DriverAutomation to $DriverAutomationOld"
-    
-            Rename-Item -LiteralPath "$DriverAutomation" -NewName "$DriverAutomationOld"
-    
-            Write-Verbose "Copying $DestPath to $psscriptroot"
-    
-            Copy-Item -Path $DestPath -Destination $psscriptroot
-    
-            Write-Verbose "Removing $DriverAutomationOld"
-    
-            Remove-Item -Path $DriverAutomationOld
-    
-            Write-Verbose "Successfully updated Driver Automation"
-        }
-        catch {
-            $UpdateError = $_.Exception.Message
-            Write-Error $UpdateError
-    
-            if (Test-Path $DestPath) {
-                Write-Verbose "Removing $DestPath"
-                Remove-Item $DestPath
-            }
-    
-            if (!(Test-Path $DriverAutomation) -and (Test-Path $DriverAutomationOld)) {
-                Write-Verbose "Renaming $DriverAutomationOld to $DriverAutomation"
-                Rename-Item -Path $DriverAutomationOld -NewName $DriverAutomation
-            }
-    
-            return $false
-        }
-        
-        return $true
-    }
-
     #----------------------------------------------------------[Declarations]----------------------------------------------------------
     $ListOfManufacturers = Get-ManufacturersInformation
     $ListOfManufacturers = @()
@@ -300,20 +265,16 @@ BEGIN {
 
     $Architectures = @("x64", "x86")
 
-    $NormalizedScriptVersion = New-Object System.Version((Test-ScriptFileInfo -Path $PSCommandPath).version)
-
     #-----------------------------------------------------------[Execution]------------------------------------------------------------
-
-    if (Update-DriverAutomation -NormalizedScriptVersion $NormalizedScriptVersion) {
-        #Restart script with same params
-    }
-    else {
-        Write-Host "No updates found."
-    }
 
     $Models = $ListOfManufacturers | Select-Object Manufacturer, @{N = "Models"; E = { $_.Drivers.SystemName } }
 
     $SelectedBuild = ConvertTo-BuildNumber -OperatingSystem $OsVersion -Version $Build
+
+    if (Update-DriverAutomation) {
+        # Rerun script with same parameters
+    }
+
 }
 
 PROCESS {
